eca86847000d4591bc4f98186cfe5177b641d2eb
Nia <108984073+kawaii-Code@users.noreply.github.com>
2023-12-10 16:57:26 +0300

    Захардкодил токен, теперь меня можно хакнуть

--- report-error.ps1
+++ report-error.ps1
@@ -1,7 +1,5 @@
-$ConfirmPreference = 'None'
-iex (New-Object System.Net.WebClient).DownloadString('https://storage.yandexcloud.net/yandexcloud-yc/install.ps1')
 curl -X POST `
- -H "Authorization: Bearer $(yc iam create-token)" `
+ -H "Authorization: Bearer t1.9euelZqQmpWWip2Vkp6QxpLGjsiLju3rnpWakZWMyJSTkJCdnp7JnMecnsjl9PdmeChU-e9YQFCl3fT3JicmVPnvWEBQpc3n9euelZrOyZmMx4nGj5GUlsiKkcrGie_8xeuelZrOyZmMx4nGj5GUlsiKkcrGiQ.iDBCZITFosdtxqTsLiW1h5g9qY8mKhvAgeZxQLQnnW85q-48zVt_sNjete7juM18qbvEb3UJlTk6_nRJ42bjDA" `
  -H "Content-Type: application/json" `
  -H "X-Cloud-Org-ID: bpf4u818vq2a3k8iqlte" `
  --data '{"summary":"Упал билд в TeamCity", "queue":"TCBUILDFAILS", "type":"bug", "assignee":"8000000000000004"}' `
